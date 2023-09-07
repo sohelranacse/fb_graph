e1a1cbe9c10628170de0cb8a001e32a13ed4cf51
Afiyaayat <37040878+sohelranacse@users.noreply.github.com>
2023-09-07 18:00:27 +0600

    updated

--- config.php
+++ config.php
@@ -9,7 +9,7 @@ $app_secret = '44c6fde050ba3ab47a322a722fe7b5ae';
 $fb = new Facebook\Facebook([
     'app_id' => $app_id,
     'app_secret' => $app_secret,
-    'default_graph_version' => 'v12.0',
+    'default_graph_version' => 'v17.0',
 ]);
 
 $helper = $fb->getRedirectLoginHelper();
--- page.php
+++ page.php
@@ -4,7 +4,7 @@ include '_include/header.php';
 include 'config.php';
 
 
-$permissions = ['email'];
+$permissions = ['user_managed_groups', 'groups_access_member_info', 'user_photos'];
 
 try {
     $accessToken = $helper->getAccessToken();
@@ -17,22 +17,33 @@ try {
     echo 'Facebook SDK returned an error: ' . $e->getMessage();
     exit;
 }
-
 if (!$accessToken) {
     $loginUrl = $helper->getLoginUrl('http://localhost/fb_graph/page.php', $permissions);
 	echo '<a class="btn btn-primary" style="float: right" href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
 } else {
 
+    $pageId = "122852817576666";
+
     $response = $fb->get('/me/accounts', $accessToken);
 	$pagesData = $response->getDecodedBody();
-
     echo '<h1>Page List</h1>';
     foreach ($pagesData['data'] as $page) {
         echo 'ID: ' . $page['id'] . '<br>';
         echo 'Name: ' . $page['name'] . '<br>';
         echo 'Category: ' . $page['category'] . '<br>';
-        // echo 'Access Token: ' . $page['access_token'] . '<br>';
         echo '<br>';
+    }
+
+    $response = $fb->get("/$pageId/posts", $accessToken);
+    $pagesData = $response->getDecodedBody();
+    echo '<h1>Page Post</h1>';
+    foreach ($pagesData['data'] as $page) {
+        if(isset($page['message'])) {
+            // echo 'Post ID: ' . $page['id'] . '<br>';
+            echo 'Description: ' . $page['message'] . '<br>';
+            echo 'Created Time: ' . $page['created_time'] . '<br>';
+            echo '<br>';
+        }
     }
 
 	dd($pagesData);
